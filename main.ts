80453c15b48b0157448d58e829354562b6987ade
Nick Anderegg <nick@anderegg.io>
2023-03-23 14:55:12 -0400

    chore: add .editorconfig and empty readme

--- main.ts
+++ main.ts
@@ -1,48 +1,48 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 interface ULIDManagerSettings {
-	mySetting: string;
+    mySetting: string;
 }
 
 const DEFAULT_SETTINGS: ULIDManagerSettings = {
-	mySetting: 'default'
+    mySetting: 'default'
 }
 
 export default class ULIDManager extends Plugin {
-	settings: ULIDManagerSettings;
+    settings: ULIDManagerSettings;
 
-	async onload() {
-		await this.loadSettings();
-		this.addSettingTab(new ULIDManagerSettingTab(this.app, this));
+    async onload() {
+        await this.loadSettings();
+        this.addSettingTab(new ULIDManagerSettingTab(this.app, this));
 
-	}
+    }
 
-	onunload() {
+    onunload() {
 
-	}
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
 class ULIDManagerSettingTab extends PluginSettingTab {
-	plugin: ULIDManager;
+    plugin: ULIDManager;
 
-	constructor(app: App, plugin: ULIDManager) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: ULIDManager) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    display(): void {
+        const { containerEl } = this;
 
-		containerEl.empty();
+        containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Universally Unique Lexicographically Sortable Identifier (ULID) Manager'});
+        containerEl.createEl('h2', { text: 'Universally Unique Lexicographically Sortable Identifier (ULID) Manager' });
 
-	}
+    }
 }
