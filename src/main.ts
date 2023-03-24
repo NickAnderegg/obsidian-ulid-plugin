762912c98c4c768ce4da271bc63d78c35f122b42
Nick Anderegg <nick@anderegg.io>
2023-03-24 14:34:21 -0400

    feat: automatically insert ULID into frontmatter on modification

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, Stat, TFile } from 'obsidian';
 import { ulid } from 'ulid'
 interface ULIDManagerSettings {
     enableManager: boolean
     frontmatterUlidKey: string
     enableFillEmptyKey: boolean
     enableUseCreationTime: boolean
-    // enableFrontmatterInsertionOnOpen: boolean
-    // enableFrontmatterInsertionOnSave: boolean
+    enableConditionalKeyInsertion: boolean
+    frontmatterConditionalKey: string
 }
 
 const DEFAULT_SETTINGS: ULIDManagerSettings = {
@@ -14,8 +14,8 @@ const DEFAULT_SETTINGS: ULIDManagerSettings = {
     frontmatterUlidKey: "uid",
     enableFillEmptyKey: false,
     enableUseCreationTime: true,
-    // enableFrontmatterInsertionOnOpen: false,
-    // enableFrontmatterInsertionOnSave: false,
+    enableConditionalKeyInsertion: false,
+    frontmatterConditionalKey: "modified",
 }
 
 export default class ULIDManager extends Plugin {
@@ -32,6 +32,26 @@ export default class ULIDManager extends Plugin {
                 editor.replaceRange(ulid(), editor.getCursor())
             },
         })
+
+        this.registerEvent(this.app.vault.on("modify", async (file: TFile) => {
+            const ulidKey = this.settings.frontmatterUlidKey
+            const stat = await this.app.vault.adapter.stat(file.path)
+            console.log(stat)
+
+            if(this.settings.enableFillEmptyKey) {
+                this.app.fileManager.processFrontMatter(file, (frontmatter) => {
+                    const ulidValue = frontmatter[ulidKey]
+                    if(ulidValue !== undefined && (ulidValue === "" || ulidValue == null || ulidValue === "null")) {
+                        if(this.settings.enableUseCreationTime) {
+                            frontmatter[ulidKey] = ulid(fileCreationTime)
+                            console.log(frontmatter)
+                        } else {
+                            frontmatter[ulidKey] = ulid()
+                        }
+                    }
+                })
+            }
+        }))
     }
 
     onunload() {
@@ -59,11 +79,10 @@ class ULIDManagerSettingTab extends PluginSettingTab {
         const { containerEl } = this;
 
         containerEl.empty();
-
         containerEl.createEl('h2', { text: 'Universally Unique Lexicographically Sortable Identifier (ULID) Manager' });
 
         new Setting(containerEl)
-            .setName('Enable ULID Manager')
+            .setName('Enable Automatic ULID Management')
             .setDesc('Enable the automatic ULID management features of this plugin.')
             .addToggle(toggle => toggle
                 .setValue(this.plugin.settings.enableManager)
@@ -90,6 +109,8 @@ class ULIDManagerSettingTab extends PluginSettingTab {
                 })
             )
 
+        containerEl.createEl('h2', { text: 'Frontmatter ULID Management' })
+
         new Setting(containerEl)
             .setName('ULID Key')
             .setDesc('The key to manage for the ULID in the frontmatter of a note.')
@@ -105,13 +126,42 @@ class ULIDManagerSettingTab extends PluginSettingTab {
         new Setting(containerEl)
             .setName('Enable Empty Key Filling')
             .setDesc(
-                'When enabled, this plugin will automatically insert a ULID into an '+
-                'empty key found in the frontmatter of a note when that note is opened or created.'
+                'When enabled, this plugin will automatically insert a ULID into the '+
+                'specified key of the frontmatter of a note when that note is modified. ' +
+                'The ULID will be automatically inserted into the key if the key contains an empty string, '+
+                'null, or the string "null."'
             )
             .addToggle(toggle => toggle
                 .setValue(this.plugin.settings.enableFillEmptyKey)
                 .onChange(async (value) => {
                     this.plugin.settings.enableFillEmptyKey = value;
+                    await this.plugin.saveSettings();
+                })
+            )
+
+        new Setting(containerEl)
+            .setName('Enable Conditional Key Insertion')
+            .setDesc(
+                'When enabled, the plugin will automatically insert a ULID at the "ULID Key" '+
+                'specified above when the key specified below is present in the frontmatter of a note '+
+                'and the ULID Key is not present.'
+            )
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.enableConditionalKeyInsertion)
+                .onChange(async (value) => {
+                    this.plugin.settings.enableConditionalKeyInsertion = value;
+                    await this.plugin.saveSettings();
+                })
+            )
+
+        new Setting(containerEl)
+            .setName('Conditional Insertion Trigger Key')
+            .setDesc('The presence of this key will trigger the insertion of a ULID at the "ULID Key" field in the frontmatter.')
+            .addText(text => text
+                .setPlaceholder('Enter the key to look for')
+                .setValue(this.plugin.settings.frontmatterConditionalKey)
+                .onChange(async (value) => {
+                    this.plugin.settings.frontmatterConditionalKey = value;
                     await this.plugin.saveSettings();
                 })
             )
